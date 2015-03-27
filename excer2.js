8dc90684ef617bb1c63f8539b1a36f3ff747c4da
mirokuxy <mirokuxy@gmail.com>
2015-03-26 21:16:37 -0700

    remove old files

--- excer2.js
+++
@@ -1,9 +0,0 @@
-//console.log(process.argv);
-
-var sum = 0;
-
-var len = process.argv.length;
-for(var j=2;j<len;j++)
-	sum += parseFloat(process.argv[j]);
-
-console.log(sum);
--- excer4.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-
-// add 'utf8' to automatically do toString()
-fs.readFile(process.argv[2],'utf8',function(err,file){
-	var lines = file.split('\n').length - 1;
-	console.log(lines);
-});
